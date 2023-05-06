2b2a2f68b99fd159dabd8dae38443a2ffb356c78
Servoo <sarver1804@gmail.com>
2023-05-07 02:30:47 +0300

    Появился разрешенный ip

--- bot/bot/settings.py
+++ bot/bot/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)imfyi37y6!#de=_z%7f1*_h9pelbb4!%_n0(7k%v#o%c&3w8^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['84.252.138.253', '127.0.0.1', 'localhost']
 
 # Application definition
 
